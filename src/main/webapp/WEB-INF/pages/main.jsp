37ee38e1b3c6e249915f86dd3224ed9965621789
Administrator <Administrator@WIN7-20140616XN>
2017-11-02 16:52:04 +0800

    insert

--- src/main/webapp/WEB-INF/pages/main.jsp
+++ src/main/webapp/WEB-INF/pages/main.jsp
@@ -1,5 +1,6 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
@@ -7,6 +8,9 @@
 <title>Insert title here</title>
 </head>
 <body>
-	main
+	${insertEmp}
+	<form action="/login/main" method="post">
+		<input type="submit" value="新增"/>
+	</form>
 </body>
 </html>
--- src/test/java/MybatisTest.java
+++ src/test/java/MybatisTest.java
@@ -9,6 +9,7 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import com.tl.login.mapper.AdminMapper;
+import com.tl.login.mapper.EmployeeMapper;
 import com.tl.login.pojo.Admin;
 
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -16,19 +17,19 @@ import com.tl.login.pojo.Admin;
 public class MybatisTest {
 
 	@Autowired
-	private AdminMapper adminMapper;
+	private EmployeeMapper employeeMapper;
 
-	@Test
+	/*@Test
 	public void testAdd() {
 		Admin admin = new Admin();
 		admin.setAdm_count("adm_count");
 		adminMapper.add(admin);
-	}
+	}*/
 
 	@Test
 	public void testList() {
-		System.out.println(adminMapper);
-		List<Admin> cs=adminMapper.list();
+		System.out.println(employeeMapper);
+		List<Admin> cs=employeeMapper.list();
 		for (Admin c : cs) {
 			System.out.println(c.getAdm_count());
 		}
